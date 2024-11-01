18012ff48a48d810fa61145b57cbe11bda0c94cd
NahNathan <nathanrodriguessantos3@gmail.com>
2024-11-01 16:11:37 -0300

    Commit Inicial

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2") 
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.withType<Test> {
+    enabled = false /
+}
+
+sourceSets {
+    main {
+        kotlin.srcDirs("src/main/kotlin")
+    }
+}
