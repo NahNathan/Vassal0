b9a2d5689c818527f1798f69ce12b714fe27fae2
NahNathan <nathanrodriguessantos3@gmail.com>
2025-05-12 09:56:09 -0300

    Comando de Ajuda

--- app/src/main/kotlin/commands/helpCommand.kt
+++ app/src/main/kotlin/commands/helpCommand.kt
@@ -1,18 +1,24 @@
 package commands
+
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
 class helpCommand {
     fun execute(event: MessageReceivedEvent) {
-        event.channel.sendMessage("Bem vindo ao Vassal0, estou ao seu comando \n").queue()
-        event.channel.sendMessage("Para enviar algum comando, use o '>vai ' seguido por algum comando: \n").queue()
-        event.channel.sendMessage(">vai { boa } \n").queue()
-        event.channel.sendMessage(">vai { evento } \n").queue()
-        event.channel.sendMessage(">vai { frenzy } { Dados A Serem Rolados } \n").queue()
-        event.channel.sendMessage(">vai { fome } \n").queue()
-        event.channel.sendMessage(">vai { F } \n").queue()
-        event.channel.sendMessage(">vai { help } \n").queue()
-        event.channel.sendMessage(">vai { resson } \n").queue()
-        event.channel.sendMessage(">vai { role { Dados A Serem Rolados } { Dados de Fome }} \n").queue()
-        event.channel.sendMessage(">vai { :skull: } \n").queue()
+        val helpMessage = """
+            🦇 Bem-vindo ao Vassal0, estou ao seu comando
+            
+            Use **>vai** seguido de um comando:
+            **>vai { boa }** – o Vassal0 te dá um 'boa noite'
+            **>vai { evento }** – gera um evento noturno aleatório
+            **>vai { frenzy } {dados}** – faz um teste para ver se seu vampiro entra em Frenezi
+            **>vai { fome }** – faz um teste de despertar, para ver se sua fome irá aumentar
+            **>vai { F }** – presta seus respeitos
+            **>vai { help }** – mostra a lista de comandos
+            **>vai { resson }** – determina a ressonância do sangue de um mortal aleatório
+            **>vai { role } {dados} {dados de fome}** – faz uma rolada normal, considerando sua fome
+            **>vai { :skull: }** – Squeleto
+        """.trimIndent()
+
+        event.channel.sendMessage(helpMessage).queue()
     }
 }
